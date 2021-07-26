4aa7d8c4176722031c9fc0f63eced66952bef5c9
HuynhLVC <73485358+HuynhLVC@users.noreply.github.com>
2021-07-26 12:27:03 +0700

    Update ui.py

--- ui.py
+++ ui.py
@@ -1259,6 +1259,6 @@ ui.bind_class("Entry", "<Button-3><ButtonRelease-3>", show_textmenu)
 ui.bind_class("Entry", "<Control-a>", callback_select_all)
 ##End_Paste_Entry
 ##Update&Upgrade:
-version = "version_1.2.1"
+version = "version_2.1.1"
 
 ui.mainloop()
